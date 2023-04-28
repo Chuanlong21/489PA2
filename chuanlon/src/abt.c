0f9510b38bfd7f67f5c347820c75e30f81043df6
chuanlon <chuanlon@buffalo.edu>
2023-04-27 21:34:59 -0400

    Update

--- chuanlon/src/abt.c
+++ chuanlon/src/abt.c
@@ -1,4 +1,6 @@
 #include "../include/simulator.h"
+#include "string.h"
+#include "stdlib.h"
 
 /* ******************************************************************
  ALTERNATING BIT AND GO-BACK-N NETWORK EMULATOR: VERSION 1.1  J.F.Kurose
@@ -14,24 +16,52 @@
 **********************************************************************/
 
 /********* STUDENTS WRITE THE NEXT SIX ROUTINES *********/
+int ackNum = 0;
+struct pkt H_packet;
+int rev = 0;
+int seq = 0;
+char * buffer;
+
+int AddCheckSum(struct pkt packet){
+    int sum = packet.seqnum + packet.acknum;
+    for (int i = 0; i < 20; ++i) {
+        sum += packet.payload[i];
+    }
+    return sum;
+}
 
 /* called from layer 5, passed the data to be sent to other side */
 void A_output(message)
   struct msg message;
 {
-
+        strncpy(H_packet.payload, message.data,20);
+        strncpy(buffer,message.data,20);
+        H_packet.acknum = ackNum;
+        H_packet.seqnum = seq;
+        H_packet.checksum = AddCheckSum(H_packet);
+        starttimer(0,5);
+        tolayer3(0,H_packet);
 }
 
 /* called from layer 3, when a packet arrives for layer 4 */
 void A_input(packet)
   struct pkt packet;
 {
+    stoptimer(1);
+    if (packet.acknum != ackNum){
+
+    }
+    if (ackNum == 0) ackNum = 1;
+    else ackNum = 0;
 
 }
 
 /* called when A's timer goes off */
 void A_timerinterrupt()
 {
+    //处理超时
+    tolayer3(0,H_packet);
+
 
 }  
 
@@ -39,7 +69,7 @@ void A_timerinterrupt()
 /* entity A routines are called. You can use it to do any initialization */
 void A_init()
 {
-
+    ackNum = 0;
 }
 
 /* Note that with simplex transfer from a-to-B, there is no B_output() */
@@ -48,7 +78,13 @@ void A_init()
 void B_input(packet)
   struct pkt packet;
 {
-
+    stoptimer(0);
+    int checksum = AddCheckSum(packet);
+    if(checksum == packet.checksum){
+        starttimer(1,5);
+        tolayer3(1,H_packet);
+        tolayer5(1,packet.payload);
+    }
 }
 
 /* the following routine will be called once (only) before any other */
